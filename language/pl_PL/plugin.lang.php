4ae28295da280c121b97bbb40b12b045f029fb59
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-25 19:06:53 +0000

    [photo_update] Update pl_PL, thanks to : T00kie
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24637 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Zdjęcie zostało zaktualizowane';
 $lang['Photo Update'] = 'Aktualizuj zdjęcie';
 $lang['Update'] = 'Aktualizacja';
+$lang['File to update'] = 'Plik do zaktualizowania';
+$lang['main file'] = 'głowny plik';
+$lang['representative picture'] = 'zdjęcie reprezentacyjne';
 ?>
