34c1b294dddb2824bc449df26623bc985b4a6409
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-25 17:24:05 +0000

    [photo_update] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24631 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Foto ticis atjaunināts';
 $lang['Photo Update'] = 'Foto Atjaunināšana';
 $lang['Update'] = 'Atjaunināt';
+$lang['representative picture'] = 'pārstāvošā bilde';
+$lang['File to update'] = 'Jaunināmā datne';
+$lang['main file'] = 'galvenā datne';
 ?>
