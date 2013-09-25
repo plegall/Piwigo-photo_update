693f2418f4596cad23e8ee70fb882b8eaaa6f560
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-25 17:24:09 +0000

    [photo_update] Update de_DE, thanks to : fs101299
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24632 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Das Foto wurde aktualisiert'; 
 $lang['Photo Update'] = 'Fotoaktualisierung'; 
 $lang['Update'] = 'Aktualisiere';
+$lang['File to update'] = 'Datei zum Updaten';
+$lang['main file'] = 'Hauptdatei';
+$lang['representative picture'] = 'representatives Bild';
 ?>
