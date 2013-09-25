1dda9f2d6a97245282b7fe830f6c55918217ce70
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-25 15:57:25 +0000

    [photo_update] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24626 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Изображение обновлено';
 $lang['Photo Update'] = 'Обновление изображения';
 $lang['Update'] = 'Обновление';
+$lang['File to update'] = 'Файл для обновления';
+$lang['main file'] = 'основной файл';
+$lang['representative picture'] = 'картинка-представитель';
 ?>
