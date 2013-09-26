68a6c653e6ab290ce6d2210ede34eefaaf7bc506
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-26 17:37:14 +0000

    [photo_update] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24652 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Fotoğraf güncellendi';
 $lang['Photo Update'] = 'Fotoğraf Güncelleme';
 $lang['Update'] = 'Güncelle';
+$lang['File to update'] = 'Güncellenecek dosya';
+$lang['main file'] = 'ana dosya';
+$lang['representative picture'] = 'temsili resim';
 ?>
