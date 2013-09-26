736f2f7614c2e2dfc2ba8b4f97b13174110050a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-26 17:37:13 +0000

    [photo_update] Update cs_CZ, thanks to : Dejv92
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24651 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Fotografie byla aktualizována';
 $lang['Photo Update'] = 'Aktualizace fotografie';
 $lang['Update'] = 'Aktualizovat';
+$lang['File to update'] = 'Soubor k nahrání';
+$lang['main file'] = 'hlavní soubor';
+$lang['representative picture'] = 'reprezentativní obrázek';
 ?>
