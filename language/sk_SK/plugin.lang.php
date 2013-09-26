62fdde7052db40795f96378d19da12e582ffc884
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-26 17:37:16 +0000

    [photo_update] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24653 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Fotka bola aktualizovaná';
 $lang['Photo Update'] = 'Aktualizácia fotky';
 $lang['Update'] = 'Aktualizácia';
+$lang['representative picture'] = 'Reprezentatívna fotka';
+$lang['main file'] = 'Môj súbor';
+$lang['File to update'] = 'Súbor na aktualizáciu';
 ?>
