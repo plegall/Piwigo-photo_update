324f79d1e6facd285551191f6a081d498d0bfd72
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-29 06:22:29 +0000

    [photo_update] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24666 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Sikeres képfrissítés';
 $lang['Photo Update'] = 'Kép cseréje';
 $lang['Update'] = 'Feltöltés';
+$lang['representative picture'] = 'reprezentatív kép';
+$lang['File to update'] = 'Fájl frissítése';
+$lang['main file'] = 'fő fájl';
 ?>
