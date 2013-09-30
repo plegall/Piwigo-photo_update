1c1e9f56e46fbebc20a5a4c4cb72f2e048cb710b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-30 19:53:51 +0000

    [photo_update] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24713 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/vi_VN/plugin.lang.php
+++ language/vi_VN/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Hình đã được cập nhật';
 $lang['Photo Update'] = 'Cập nhật hình';
 $lang['Update'] = 'Cập nhật';
+$lang['File to update'] = 'File cần cập nhật';
+$lang['main file'] = 'file chính';
+$lang['representative picture'] = 'hình đại diện';
 ?>
