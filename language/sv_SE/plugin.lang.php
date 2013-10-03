47e1ad26ca8162f3c97b5f9ee4d896511d47a96a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-03 21:01:40 +0000

    [photo_update] Update sv_SE, thanks to : kabo
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24744 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sv_SE/plugin.lang.php
+++ language/sv_SE/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'Fotot uppdaterades';
 $lang['Photo Update'] = 'Fotouppdatering';
 $lang['Update'] = 'Uppdatera';
+$lang['File to update'] = 'Fil att uppdatera';
+$lang['main file'] = 'huvudfil';
+$lang['representative picture'] = 'representativ bild';
 ?>
