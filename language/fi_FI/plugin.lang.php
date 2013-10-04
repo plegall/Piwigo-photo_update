cdd947f4629c82d7d1090a3cd386f0ff7c0afb8a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-04 16:15:23 +0000

    [photo_update] Update fi_FI, thanks to : Juhi24
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24754 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fi_FI/plugin.lang.php
+++ language/fi_FI/plugin.lang.php
@@ -4,4 +4,5 @@ $lang['Photo Update'] = 'Kuvan päivitys';
 $lang['Update'] = 'Päivitys';
 $lang['File to update'] = 'Päivitettävä tiedosto';
 $lang['main file'] = 'ensisijainen tiedosto';
+$lang['representative picture'] = 'edustava kuva';
 ?>
