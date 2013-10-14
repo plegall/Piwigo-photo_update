0eff442d3b1b381d3fea721b3247a9e38bc4236f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-14 04:26:32 +0000

    [photo_update] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@24887 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -2,4 +2,7 @@
 $lang['The photo was updated'] = 'De foto is bijgewerkt';
 $lang['Photo Update'] = 'Foto bijwerken';
 $lang['Update'] = 'Bijwerken';
+$lang['File to update'] = 'Bij te werken bestand';
+$lang['main file'] = 'Hoofdbestand';
+$lang['representative picture'] = 'representatieve afbeelding';
 ?>
