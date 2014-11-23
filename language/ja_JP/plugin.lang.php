269929a7c3a22d2f95523522337c6b0c3a859bd4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-23 07:43:19 +0000

    [photo_update] Add ja_JP, thanks to ma2tak
    git-svn-id: http://piwigo.org/svn/extensions/photo_update@30575 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['File to update'] = '更新対象ファイル';
+$lang['Photo Update'] = '更新対象写真';
+$lang['The photo was updated'] = '写真は、更新されました';
+$lang['Update'] = '更新';
+$lang['main file'] = 'メインファイル';
+$lang['representative picture'] = '代表写真';
