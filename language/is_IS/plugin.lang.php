e489386a506f2fe3bfda7ab1e37f0e3b6a787cdb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-08 12:58:15 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['File to update'] = 'Skrá til að endurnýja';
+$lang['Photo Update'] = 'Mynd Endurnýjun';
+$lang['The photo was updated'] = 'Myndin var endurnýjuð';
+$lang['Update'] = 'Endurnýja';
+$lang['main file'] = 'aðalskrá';
+$lang['representative picture'] = 'einkennismynd';
